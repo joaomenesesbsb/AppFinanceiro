c052bb385c099c4d5e7f4e3214158ab9fc6db874
Joao Victor <joaovic2012@hotmail.com>
2025-04-10 18:33:41 -0300

    Criada a classe transaction

--- src/main/java/com/appfinanceiro/model/Transaction.java
+++ src/main/java/com/appfinanceiro/model/Transaction.java
@@ -1,5 +1,25 @@
 package com.appfinanceiro.model;
 
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
 public class Transaction {
+
+    private Long id;
+    private String description; // descrição do lançamento
+    private double amount;      // valor em reais
+    private String type;        // Receita ou Despesa
+    private LocalDate date;     // data da transação
+
+    public Transaction() {}
+
+    public Transaction(String description, double amount, String type, LocalDate date) {
+        this.description = description;
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
 
 }
